325a4145fd0f24e600f8093c0483770ac28c93d5
gingerGarden <gooopylim@gmail.com>
2024-12-13 23:22:42 +0900

    v0.1.9 utils.data.get done

--- Garden/utils/data/get.py
+++ Garden/utils/data/get.py
@@ -1,33 +1,73 @@
-from typing import Optional, Dict, Union, List
+from typing import Optional, Dict, Union, List, Tuple, Any
 
 import os
 import zipfile
-from ..path import load_pickle, new_dir_maker, file_download, unzip
+import pandas as pd
+from ..path import load_pickle, read_json, new_dir_maker, file_download, unzip
 
+
+class _TabularAction:
+    @classmethod
+    def run(cls, data_path: str) -> Dict[str, Union[str, pd.DataFrame]]:
+        data = load_pickle(data_path)
+        info = data['metadata']
+        data = data['data']
+        return info, data
+    
+    
+    
+class _COCOAction:
+    source_fils = "sample_annotaions.json"
+    image_dir = "sample_images"
+    
+    @classmethod
+    def run(cls, data_path: str):
+        # source를 가져온다.
+        source = read_json(os.path.join(data_path, cls.source_fils))
+        
+        # info
+        source['info']['licenses'] = source['licenses']
+        info = source['info']
+        
+        # data
+        data = cls._make_data(
+            parents_path=os.path.join(data_path, cls.image_dir),
+            source=source
+        )
+        return info, data
+    
+    @classmethod
+    def _make_data(cls, parents_path: str, source: Dict[str, Any]):
+        for record_dict in source['images']:
+            record_dict['file_name'] = os.path.join(parents_path, record_dict['file_name'])
+            
+        return {
+            "images":source['images'], 
+            "annotation":source['annotation'], 
+            "categories":source['categories'], 
+        }
+        
+        
 
 class Get:
     data_dict = {
         "student-mat(tabular)":{
-            "info":{"type":"file"},
-            "urls":{"url1":"https://github.com/gingerGarden/Garden/raw/refs/heads/main/sample_data/tabular/student-mat.pickle"}
+            "info":{"type":"file", "method":_TabularAction},
+            "urls":{"url1":"https://github.com/gingerGarden/Garden/raw/refs/heads/main/sample_data/tabular/student-mat.pickle"},
         },
         "student-por(tabular)":{
-            "info":{"type":"file"},
+            "info":{"type":"file", "method":_TabularAction},
             "urls":{"url1":"https://github.com/gingerGarden/Garden/raw/refs/heads/main/sample_data/tabular/student-por.pickle"}
         },
         "coco(images)":{
-            "info":{"type":"directory", "dir_name":"coco"},
+            "info":{"type":"directory", "dir_name":"coco", "method":_COCOAction},
             "urls":{
                 "url1":"https://github.com/gingerGarden/Garden/raw/refs/heads/main/sample_data/image/coco/sample_images.zip",
                 "url2":"https://github.com/gingerGarden/Garden/raw/refs/heads/main/sample_data/image/coco/sample_annotaions.json"
             }
         }
     }
-    tabular_key = {
-        "metadata":"metadata",
-        "attribute":"attribute",
-        "data":"data"
-    }
+
     # 경로 정보
     home_directory_path = os.path.expanduser("~")
     library_dir = ".garden"
@@ -35,7 +75,22 @@ class Get:
     parents_path = f"{home_directory_path}/{library_dir}/{data_dir}"
     
     @classmethod
-    def show_data_list(cls) -> List[str]:
+    def sample_data(cls, key: str, remove_old: bool) -> Tuple[Dict, Dict]:
+        # key가 지원 데이터 안에 포함되는지 확인
+        if key not in list(cls.data_dict.keys()):
+            raise ValueError(f"입력한 {key}는 샘플 데이터로 제공하지 않습니다. Get.show_sample_list()로 제공 데이터를 확인하십시오.")
+        
+        # 데이터 다운로드
+        data_path = cls._get_data_path(key=key)
+        if remove_old or not os.path.exists(data_path):
+            cls._download_sample_data(key=key, remove_old=remove_old)
+            
+        # 데이터를 가져온다.
+        info, data = cls.data_dict[key]['info']['method'].run(data_path)
+        return info, data
+                
+    @classmethod
+    def show_sample_list(cls) -> List[str]:
         """
         Garden에서 지원하는 샘플 데이터의 key list를 반환한다.
 
@@ -45,7 +100,24 @@ class Get:
         return list(cls.data_dict.keys())
     
     @classmethod
-    def download_sample_data(cls, key: str, remove_old: bool):
+    def _get_data_path(cls, key: str) -> str:
+        """
+        key의 데이터에 대해, 생성되는 대표 데이터 경로 출력
+
+        Args:
+            key (str): sample data의 key
+
+        Returns:
+            str: 대표 데이터 경로
+        """
+        if cls.data_dict[key]['info']['type'] == "directory":
+            return os.path.join(cls.parents_path, cls.data_dict[key]['info']['dir_name'])
+        else:
+            file_name = cls.data_dict[key]['urls']['url1'].split("/")[-1]
+            return os.path.join(cls.parents_path, file_name)
+    
+    @classmethod
+    def _download_sample_data(cls, key: str, remove_old: bool):
         """
         key에 해당하는 샘플 데이터를 다운로드한다.
             - 데이터가 다운로드될 부모 경로가 존재하지 않는 경우, 이를 제거한다.
@@ -70,20 +142,33 @@ class Get:
         """
         데이터 다운로드를 위한 가장 기본적인 디렉터리들을 생성한다.
         """
-        new_dir_maker(f"{cls.home_directory_path}/{cls.library_dir}", makes_new=False)
+        new_dir_maker(os.path.join(cls.home_directory_path, cls.library_dir), makes_new=False)
         new_dir_maker(cls.parents_path, makes_new=False)
-    
+
     
     
 class _SampleDataDownload:
     def __init__(self, parents_path: str, key_dict: Dict[str, str], remove_old: bool):
+        """
+        Sample data를 다운로드 받는다.
+            - Sample data는 해당 코드의 github 레포지토리의 sample_data 디렉터리 하위에 존재한다.
+
+        Args:
+            parents_path (str): 데이터가 다운로드 될 부모 디렉터리 경로
+            key_dict (Dict[str, str]): 데이터 다운로드의 key가 되는 값들이 존재하는 딕셔너리
+            remove_old (bool): 기존 데이터가 존재한다면, 이를 제거하고 새로 받을지 여부
+        """
         self.parents_path = parents_path
         self.key_dict = key_dict
         self.remove_old = remove_old
         self.data_dir_path = None
         
     def run(self):
-        
+        """
+        Sample data를 다운로드 하는 프로세스 전체를 실행한다.
+            - directory type과 그렇지 않은 타입은 별도로 진행된다.
+            - directory type은 디렉터리를 생성 후, 하위 데이터 파일들을 다운로드하고, zip 파일이 있는 경우 압축 해제한다.
+        """
         if self.key_dict['info']['type'] == 'directory':
             # 데이터가 다운로드될 하위 디렉터리 생성
             self._make_sub_directory()
@@ -140,102 +225,6 @@ class _SampleDataDownload:
         """
         # url을 기반으로 파일 경로를 생성한다.
         file_name = url.split("/")[-1]
-        file_path = f"{dir_path}/{file_name}"
+        file_path = os.path.join(dir_path, file_name)
         # file 다운로드 및 다운로드 완료 여부 출력
         return file_download(url, local_path=file_path, remove_old=self.remove_old)
-        
-        
-        
-
-# class _Old:
-#     @classmethod
-#     def load(
-#             cls, data_name: Optional[str] = None
-#         ) -> Dict[str, Dict[str, Union[str, pd.DataFrame]]]:
-#         """
-#         data_name에 해당하는 모든 데이터(메타 데이터, 데이터)를 가져온다.
-
-#         Args:
-#             data_name (Optional[str], optional): 데이터 이름. Defaults to None.
-#                 - None이 입력되는 경우, 기본 값인 "student(math)"를 출력한다.
-
-#         Returns:
-#             Dict[str, Dict[str, Union[str, pd.DataFrame]]]: 데이터
-#         """
-#         if data_name is None:
-#             warnings.warn("key에 아무 값이 입력되지 않았습니다. default인 student(math) 데이터를 가져옵니다.")
-#             data_name = "student(math)"
-#         return load_pickle(pickle_path=cls.path_dict[data_name])
-    
-#     @classmethod
-#     def data(cls, data_name: str) -> pd.DataFrame:
-#         """
-#         data_name에 해당하는 데이터만 가져온다.
-
-#         Args:
-#             data_name (str): 데이터 이름
-
-#         Returns:
-#             pd.DataFrame: 데이터
-#         """
-#         data = load_pickle(pickle_path=cls.path_dict[data_name])
-#         return data[cls.data_key]
-    
-#     @classmethod
-#     def metadata(cls, data_name: str, verbose: bool = False) -> Dict[str, str]:
-#         """
-#         data_name에 해당하는 metadata를 가져온다.
-
-#         Args:
-#             data_name (str): 데이터 이름
-#             verbose (bool, optional): pprint로 보기 좋게 출력할지 여부. Defaults to False.
-
-#         Returns:
-#             Dict[str, str]: metadata의 dictionary
-#         """
-#         data = load_pickle(pickle_path=cls.path_dict[data_name])
-#         return cls._get_target_value(data=data, key=cls.metadata_key, verbose=verbose)
-    
-#     @classmethod
-#     def attribute(cls, data_name: str, verbose: bool = False) -> Dict[str, str]:
-#         """
-#         data_name에 해당하는 attribute를 가져온다.
-
-#         Args:
-#             data_name (str): 데이터 이름
-#             verbose (bool, optional): pprint로 보기 좋게 출력할지 여부. Defaults to False.
-
-#         Returns:
-#             Dict[str, str]: attribute의 dictionary
-#         """
-#         data = load_pickle(pickle_path=cls.path_dict[data_name])
-#         return cls._get_target_value(data=data, key=cls.attribute_key, verbose=verbose)
-
-#     @classmethod
-#     def data_name_list(cls) -> List[str]:
-#         """
-#         sample data의 data name list를 출력한다.
-
-#         Returns:
-#             List[str]: data name의 List
-#         """
-#         return list(cls.path_dict.keys())
-    
-#     @classmethod
-#     def _get_target_value(cls, data: Dict[str, str], key: str, verbose: bool) -> Dict[str, str]:
-#         """
-#         cls.metadata 와 cls.attribute의 하위 메서드
-#             - data의 dictionary에서 key에 해당하는 결과를 가져오고, pprint로 출력할지 여부를 정한다.
-
-#         Args:
-#             data (Dict[str, str]): metadata, attribute, data 3개의 key로 이루어진 데이터(dict)
-#             key (str): data(dict)에서 출력하고자 하는 key
-#             verbose (bool): pprint로 보기 좋게 출력할지 여부
-
-#         Returns:
-#             Dict[str, str]: key에 해당하는 데이터
-#         """
-#         result = data[key]
-#         if verbose:
-#             pprint(result)
-#         return result
